79bbfbc6423a8378fb42ae8ad1343e7d2b0b8215
Abdulazeez Ismail <aismail@creativeadvtech.com>
2022-10-03 07:41:44 +0100

    add FeatureFlagEnum.ANDROID_SCREENSHARING_ENABLED:         'android.screensharing.enabled',

--- jitsi_meet_platform_interface/lib/feature_flag/feature_flag_helper.dart
+++ jitsi_meet_platform_interface/lib/feature_flag/feature_flag_helper.dart
@@ -21,5 +21,7 @@ class FeatureFlagHelper {
     FeatureFlagEnum.TILE_VIEW_ENABLED: 'tile-view.enabled',
     FeatureFlagEnum.TOOLBOX_ALWAYS_VISIBLE: 'toolbox.alwaysVisible',
     FeatureFlagEnum.WELCOME_PAGE_ENABLED: 'welcomepage.enabled',
+    FeatureFlagEnum.ANDROID_SCREENSHARING_ENABLED:
+        'android.screensharing.enabled',
   };
 }
